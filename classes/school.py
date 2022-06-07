fffd8599865475d8acfc50eb3569676558451ac2
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-06-06 18:09:21 -0700

    COMP

--- classes/school.py
+++ classes/school.py
@@ -1,3 +1,5 @@
+from cmd import PROMPT
+from urllib import response
 from classes.staff import Staff
 from classes.student import Student
 
@@ -6,3 +8,16 @@ class School:
         self.name = name
         self.staff = Staff.objects()
         self.students = Student.objects()
+        
+    def all_studs(self):
+        for idx,stud in enumerate(self.students):
+            print(f"{idx+1}. {stud.name}, {stud.school_id}")
+            
+    def find_stud_id(self, school_id):
+        for stud in self.students:
+            if school_id == stud.school_id:
+                print(f"{stud.name}")
+                print('---------------')
+                print(f"age:{stud.age}")
+                print(f"id:{stud.school_id}")
+        
--- runner.py
+++ runner.py
@@ -1,8 +1,21 @@
+from tokenize import String
 from classes.school import School
 
 school = School('Ridgemont High')
 
 
-print(school.staff)
-print(school.students)
+
+#runner.py
+menu="\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n"
+res=input(f"{menu}")
+
+while res !='5':
+    if res=='1':
+        school.all_studs()
+        print(res)
+    elif res=='2':
+        stud_id=input('Enter students ID')
+        student=school.find_stud_id(stud_id) 
+        res=input(menu)  
+
 
